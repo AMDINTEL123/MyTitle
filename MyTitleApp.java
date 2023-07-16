065b0a8720c7123e3b05793c379c95da8792c2d7
AMDINTEL123 <136127865+AMDINTEL123@users.noreply.github.com>
2023-07-16 21:36:14 +0200

    Add files via upload

---
+++ MyTitleApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mytitleapp;
+
+import za.ac.tut.ui.MyTitle;
+
+/**
+ *
+ * @author khens
+ */
+public class MyTitleApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyTitle initial;
+                
+        initial=new MyTitle();
+    }
+    
+}
